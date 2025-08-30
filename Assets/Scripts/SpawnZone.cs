b05906b85c419c8b74bec89d9e78a0ff85621fe1
Hamilton Posada Serna <70818849+HamiltonPDev@users.noreply.github.com>
2025-08-30 19:25:41 +0200

    i have been created the spawnzone for the player follow

---
+++ Assets/Scripts/SpawnZone.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* This script is for spawning designed zones */
+
+public class SpawnZone : MonoBehaviour
+{
+    private PlayerController thePlayer;
+    private CameraFollow theCamera;
+
+    void Start()
+    {
+        thePlayer = FindFirstObjectByType<PlayerController>();
+        theCamera = FindFirstObjectByType<CameraFollow>();
+
+        thePlayer.transform.position = this.transform.position;
+        theCamera.transform.position = new Vector3(
+            this.transform.position.x,
+            this.transform.position.y,
+            theCamera.transform.position.z
+        );
+    }
+}
