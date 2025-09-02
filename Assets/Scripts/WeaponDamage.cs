a6872794587630bad3ad2477937d435b2145b7ad
Hamilton Posada Serna <70818849+HamiltonPDev@users.noreply.github.com>
2025-09-02 14:18:12 +0200

    feat: Update weapon damage logic to apply damage to enemies instead of destroying them

--- Assets/Scripts/WeaponDamage.cs
+++ Assets/Scripts/WeaponDamage.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class WeaponDamage : MonoBehaviour
 {
+    public int damage;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-       if (collision.gameObject.tag.Equals("Enemy"))
+        if (collision.gameObject.tag.Equals("Enemy"))
         {
-            Destroy(collision.gameObject);
+            collision.gameObject.GetComponent<HealthManager>().DamageCharacter(damage);
         }
     }
 }
