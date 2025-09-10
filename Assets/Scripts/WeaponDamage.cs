4bec460c0d5e128ae7bfd2246d2a5f1960e37d55
Hamilton Posada Serna <70818849+HamiltonPDev@users.noreply.github.com>
2025-09-10 16:26:47 +0200

    Add Blood Burst prefab with initial metadata

--- Assets/Scripts/WeaponDamage.cs
+++ Assets/Scripts/WeaponDamage.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class WeaponDamage : MonoBehaviour
 {
+    /* Damage amount */
     public int damage;
+    public GameObject hurtAnimation;
+    public GameObject hitPoint;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals("Enemy"))
         {
+            // this line applies damage to the enemy
             collision.gameObject.GetComponent<HealthManager>().DamageCharacter(damage);
+
+            // this ine creates a visual effect at the hit location
+            Instantiate(hurtAnimation, hitPoint.transform.position, hitPoint.transform.rotation);
         }
     }
 }
