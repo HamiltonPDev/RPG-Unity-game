0aaad516f2053e665aa6cd01f57d506c8b1d983a
Hamilton Posada Serna <70818849+HamiltonPDev@users.noreply.github.com>
2025-09-15 12:21:06 +0200

    feat: Enhance DamageNumber functionality and add UIManager script

--- Assets/Scripts/DamageNumber.cs
+++ Assets/Scripts/DamageNumber.cs
@@ -2,24 +2,32 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class DamageNumber : MonoBehaviour
-{
+{   
+    public static float damageAmount= 0;
     public float damagePoints;
     [SerializeField] float damageSpeed;
     [SerializeField] Text damageText;
 
+    public static void ResetDamage()
+    {
+        damageAmount = 0;
+    }
+
     // Start is called before the first frame update and destroys the damage number after 1.5 seconds
     private void Start()
     {
+        damageAmount += damagePoints; // It accumulate the damage points
         Destroy(this.gameObject, 1.5f);
     }
 
     void Update()
     {
-        damageText.text = damagePoints.ToString();
+        damageText.text = damageAmount.ToString();
         this.transform.position = new Vector3(
             this.transform.position.x,
             this.transform.position.y + (damageSpeed * Time.deltaTime),
             this.transform.position.z
         );
+
     }
 }
--- Assets/Scripts/WeaponDamage.cs
+++ Assets/Scripts/WeaponDamage.cs
@@ -8,16 +8,32 @@ public class WeaponDamage : MonoBehaviour
     public int damage;
     public GameObject hurtAnimation;
     public GameObject hitPoint;
+    public GameObject damageNumber;
+    private GameObject currentEnemy; // track current enemy
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals("Enemy"))
         {
+            // Reset damege if attacking a different enemy
+            if (currentEnemy != collision.gameObject)
+            {
+                DamageNumber.ResetDamage();
+                currentEnemy = collision.gameObject;
+            }
             // this line applies damage to the enemy
             collision.gameObject.GetComponent<HealthManager>().DamageCharacter(damage);
 
-            // this ine creates a visual effect at the hit location
+            // this line creates a visual effect at the hit location
             Instantiate(hurtAnimation, hitPoint.transform.position, hitPoint.transform.rotation);
+
+            // this line creates a damage number at the hit location
+            var clone = Instantiate(
+                damageNumber, hitPoint.transform.position,
+                Quaternion.Euler(Vector3.zero) // no rotation
+            );
+            // set the damage amount on the damage numberdama
+            clone.GetComponent<DamageNumber>().damagePoints = damage;
         }
     }
 }
