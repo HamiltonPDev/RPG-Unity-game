5b3ebeb655a69ee38d3b3f5574412f0bdfcd8244
Hamilton Posada Serna <70818849+HamiltonPDev@users.noreply.github.com>
2025-09-15 16:00:35 +0200

    Add health flashing effect and create CharacterStats script
    - Implemented a flashing effect for the character when damaged in HealthManager.cs.
    - Added coroutine to handle color flashing and disabling collider during invincibility.
    - Created a new CharacterStats.cs script as a placeholder for future character statistics functionality.

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -9,10 +9,22 @@ public class HealthManager : MonoBehaviour
     [SerializeField]
     public int currentHealth;
 
+    [Header ("IFrame stuff")]
+    public Color flashColor;
+    public Color regularColor;
+    public float flashDuration;
+    public int numberOfFlashes;
+    
+
+    private Collider2D triggerCollider;
+    private SpriteRenderer mySprite;
+
     void Start()
     {
         // Initialize current health with max health
         currentHealth = maxHealth;
+        triggerCollider = GetComponent<Collider2D>();
+        mySprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -27,6 +39,7 @@ public class HealthManager : MonoBehaviour
     /* Methods to calculate health damage */
     public void DamageCharacter(int damage)
     {
+        StartCoroutine(FlashCo());
         currentHealth -= damage;
     }
 
@@ -34,5 +47,20 @@ public class HealthManager : MonoBehaviour
     {
         maxHealth = newMaxHealth;
         currentHealth = maxHealth; // Reset current health to new max health
+    }
+
+    private IEnumerator FlashCo()
+    {
+        int temp = 0;
+        triggerCollider.enabled = false;
+        while (temp < numberOfFlashes)
+        {
+            mySprite.color = flashColor;
+            yield return new WaitForSeconds(flashDuration);
+            mySprite.color = regularColor;
+            yield return new WaitForSeconds(flashDuration);
+            temp++;
+        }
+        triggerCollider.enabled = true;
     }
 }
