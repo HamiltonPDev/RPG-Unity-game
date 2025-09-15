5e93473503c567b829f702245110f3d732207e77
Hamilton Posada Serna <70818849+HamiltonPDev@users.noreply.github.com>
2025-09-15 14:30:33 +0200

    feat: update prefabs and script to enhance damage handling and scene transitions

--- Assets/Scripts/DamagePlayer.cs
+++ Assets/Scripts/DamagePlayer.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class DamagePlayer : MonoBehaviour
 {
     public int damage;
+    public GameObject damageNumberPrefab;
 
     /* OnCollisionEnter2D is for detecting player collisions */
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        /* Damage the player */
         if (collision.gameObject.tag.Equals("Player"))
         {
+            // Apply damage to the player
             collision.gameObject.GetComponent<HealthManager>().DamageCharacter(damage);
+
+            var clone = Instantiate(
+                damageNumberPrefab, collision.transform.position,
+                Quaternion.Euler(Vector3.zero) // no rotation
+            );
+            // set the damage amount on the damage number
+            clone.GetComponent<DamageNumber>().damagePoints = damage;
         }
     }
 }
