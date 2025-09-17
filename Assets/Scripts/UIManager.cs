178ffb1f5ebb67c3563818ba530b47cb72a77783
Hamilton Posada Serna <70818849+HamiltonPDev@users.noreply.github.com>
2025-09-17 11:47:11 +0200

    feat: Organize UImanager script by grouping player health and stats elements

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,18 +7,26 @@ using System.Text;
 public class UIManager : MonoBehaviour
 {
     // References to UI elements
+    [Header ("Player Health Elements")]
     public Slider PlayerHealthBar;
     public Text PlayerHealthText;
+    public HealthManager playerHealthManager;
 
     // Reference to the player's HealthManager script
-    public HealthManager playerHealthManager;
+    [Header("Player Stats")]
+    public Text PlayerLevelText;    
+    public Text PlayerCurrentExpText;
+    public CharacterStats playerStats;
 
     void Update()
     {
+        /* update health UI */
         int maxHealth = playerHealthManager.maxHealth;
         int currentHealth = playerHealthManager.currentHealth;
 
-        // If we level up and max health changes, update the health bar's max value
+        /* Update Player stats */
+        int currentLevel = playerStats.currentLevel;
+        int currentExp = playerStats.currentExp;
 
         // Update the health bar's value and text every frame
         PlayerHealthBar.maxValue = maxHealth;
@@ -26,5 +34,9 @@ public class UIManager : MonoBehaviour
 
         /* Update the health text */
         PlayerHealthText.text = string.Format("{0} / {1}", currentHealth, maxHealth);
+
+        /* Update the player stats text */
+        PlayerLevelText.text = "Level: " + currentLevel.ToString();
+        PlayerCurrentExpText.text = "XP: " + currentExp.ToString();
     }
 }
