4fd38789cf39d8ef5fc8039a4c6c5c2f8046a2af
Hamilton Posada Serna <70818849+HamiltonPDev@users.noreply.github.com>
2025-09-30 14:13:05 +0200

    Enhancy enemy si by adding attack conditions in animator transitions, updating enemy prefab parameters for patrol and attack behavior and removing unused enemy controller script

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+    This is a learning script to handle enemy behavior and random movement. I don't use it anymore.
+*/
+
 
 /* This script controls the enemy character randomly */
 public class EnemyController : MonoBehaviour
